6654dff6daaceb058b20fe71b50817dc45930037
Hector - Xavier de Lastic <37729624+Hector-Xavier@users.noreply.github.com>
2018-10-09 16:18:33 +0300

    Update file_splitter.R

--- file_splitter.R
+++ file_splitter.R
@@ -9,9 +9,9 @@ file_splitter <- function(input_file)
   {
     write.table(input_file[i,],file=paste(i,input_file[i,1],sep="-"),col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")
     output_list <- c(output_list,paste(i,input_file[i,1],sep="-"))
-  }
-  return(output_list)
-  write.table(output_list,file="output_list",col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")  
+  }  
+  write.table(output_list,file="output_list",col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")
+  #return(output_list)
 }
 
 res = file_splitter(input_file)
