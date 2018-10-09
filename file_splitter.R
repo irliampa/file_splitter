abe1c821b0782a09c74197b320051b153905d192
Hector - Xavier de Lastic <37729624+Hector-Xavier@users.noreply.github.com>
2018-10-09 14:08:49 +0300

    Update file_splitter.R

--- file_splitter.R
+++ file_splitter.R
@@ -1,6 +1,7 @@
 file_splitter <- function(input_file)
 {
   temp <- read.table(file=input_file,sep="\t",header=FALSE)
+  setwd("/home/data")
   for (i in 1: dim(temp)[1])
     write.table(temp[i,],file=paste(i,temp[i,1],sep="-"),col.names=FALSE,row.names=FALSE,quote=FALSE)
 }
