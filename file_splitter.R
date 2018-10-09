52fc6fa14b5d2c2ef27e5d8ddc784caf4f61dda3
Hector - Xavier de Lastic <37729624+Hector-Xavier@users.noreply.github.com>
2018-10-09 13:06:47 +0300

    Add files via upload

---
+++ file_splitter.R
@@ -0,0 +1,6 @@
+traxanas <- function(input_file)
+{
+  temp <- read.table(file=input_file,sep="\t",header=FALSE)
+  for (i in 1: dim(temp)[1])
+    write.table(temp[i,],file=paste(i,temp[i,1],sep="-"),col.names=FALSE,row.names=FALSE,quote=FALSE)
+}
